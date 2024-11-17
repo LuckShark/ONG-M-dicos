75465587162c9ddb4fda41590aee5d99d34322da
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-17 18:32:29 -0300

    Swagger e configurações add

--- back/src/main/java/com/example/lucasigor/controller/VolunteerController.java
+++ back/src/main/java/com/example/lucasigor/controller/VolunteerController.java
@@ -4,6 +4,8 @@ package com.example.lucasigor.controller;
 import com.example.lucasigor.entities.Volunteer;
 import com.example.lucasigor.repositories.VolunteerRepository;
 import com.example.lucasigor.services.VolunteerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/volunteers")
+@Tag(name = "Volunteers", description = "Endpoints para gerenciar Voluntários")
 public class VolunteerController {
 
     //Injeção de dependencia
@@ -32,11 +35,13 @@ public class VolunteerController {
         return volunteerService.findById(id);
     }
     @PostMapping
+    @Operation(summary = "Cadastrar Voluntário", description = "Cria um novo voluntário")
     public Volunteer create(@Valid @RequestBody Volunteer volunteer) {
         return volunteerService.save(volunteer);
     }
 
     @PutMapping(value = "/{id}")
+    @Operation(summary = "Atualizar voluntário", description = "Atualiza os dados de um voluntário existente")
     public ResponseEntity<Volunteer> updateVolunteer(
             @PathVariable Long id,
             @Valid @RequestBody Volunteer volunteer) {
