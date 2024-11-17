f2ebe16852cf692d44507785687ff8a22ac58bb7
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-17 10:04:39 -0300

    getById implementado em voluntários

--- back/src/main/java/com/example/lucasigor/controller/VolunteerController.java
+++ back/src/main/java/com/example/lucasigor/controller/VolunteerController.java
@@ -6,6 +6,7 @@ import com.example.lucasigor.repositories.VolunteerRepository;
 import com.example.lucasigor.services.VolunteerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,10 @@ public class VolunteerController {
     @GetMapping
     public List<Volunteer> findAll() {
         return volunteerService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Volunteer findById(@PathVariable Long id) {
+        return volunteerService.findById(id);
     }
 }
--- back/src/main/java/com/example/lucasigor/services/VolunteerService.java
+++ back/src/main/java/com/example/lucasigor/services/VolunteerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VolunteerService {
@@ -16,5 +17,11 @@ public class VolunteerService {
     //GET ALL
     public List<Volunteer> findAll() {
         return volunteerRepository.findAll();
+    }
+
+    //GET BY ID
+    public Volunteer findById(Long id) {
+        Optional<Volunteer> result = volunteerRepository.findById(id);
+        return  result.orElseThrow(()-> new RuntimeException("Voluntário não encontrado"));
     }
 }
