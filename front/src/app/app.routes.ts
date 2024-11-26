d0c95ffdc65ccb5987ef800749e339c8514a63df
lucasaraujoalmeida <87248601+LuckShark@users.noreply.github.com>
2024-11-26 16:27:11 -0300

    fazer o form

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './login/login.component';
+import { FormRegisterVoluntaryComponent } from './pages/register-voluntary/form-register-voluntary/form-register-voluntary.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'register', component: FormRegisterVoluntaryComponent },
     { path: '**', component: HomeComponent },
 ];
